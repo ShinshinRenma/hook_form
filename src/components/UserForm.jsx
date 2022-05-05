6e651b337db3fdc9db0b8744f4fc7b19445db600
ShinshinRenma <ryan.andrew.layman@gmail.com>
2022-05-05 13:11:02 -1000

    first commit

---
+++ src/components/UserForm.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from  'react';
+    
+const UserForm = (props) => {
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmpassword, setConfirmPassword] = useState("");
+    
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = { firstname, lastname, email, password, confirmpassword };
+        console.log("Welcome", newUser);
+    };
+    
+    return(
+        <div>
+        <form onSubmit={ createUser }>
+            <div>
+                <label>First Name: </label> 
+                <input type="text" onChange={ (e) => setFirstname(e.target.value) } />
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" onChange={ (e) => setLastname(e.target.value) } />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="password" onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input type="password" onChange={ (e) => setConfirmPassword(e.target.value) } />
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
+        <div>
+            <h1>Your Data</h1>
+            <h6>First Name: {firstname}</h6>
+            <h6>Last Name: {lastname}</h6>
+            <h6>Email Address: {email}</h6>
+            <h6>Password: {password}</h6>
+            <h6>Confirm Password: {confirmpassword}</h6>
+        </div>
+        </div>
+        );
+};
+    
+export default UserForm;
